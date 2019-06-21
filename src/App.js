237a2e706e25c080c5cceee5f1d35c3636a3a60d
ryan <2869888186@qq.com>
2019-06-21 20:12:16 +0800

    获取input标签的值

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            inputValue: undefined,
+            arr: [],
+        }
+    }
+
+    handleSub = (e) => {
+        e.preventDefault()
+        console.log(this.state.inputValue)
+    }
+
+    handleInputChange = (e) => {
+        this.setState({
+            inputValue:e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <form onSubmit={this.handleSub}>
+                    <input
+                        onChange={this.handleInputChange}
+                    />
+                    <button
+                        type={onsubmit}
+
+                    >AddTodo
+                    </button>
+                </form>
+
+            </div>
+        );
+    }
 }
 
 export default App;
